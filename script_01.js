7157e9d3ec9f62ac7998dd1515f6a9028871f28d
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-18 12:14:41 +0200

    calculator

--- script_01.js
+++ script_01.js
@@ -7,9 +7,38 @@
 
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+3. Fkt. Grundrechenarten : Check!
 4. Ausgabe in Konsole : Check!
 */
+
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!"
+const ERROR_STR_CAL = "ungültige Eingabe!"
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/","!"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+
+function calculator (a, b, op) {
+	switch (op) {
+		case "+":
+			return addition(a, b);
+		case "-":
+			return subtraction(a, b);
+		case "*":
+			return multiplication(a, b);
+		case "/":
+		case ":":
+			return division(a, b);
+		default:
+			return ERROR_STR_CAL;
+	}
+}
 
 // module: addition a + b |  test:
 // output(addition(a, b));
@@ -31,12 +60,11 @@ function multiplication(a, b) {
 
 // module: division a / b |  test
 // output(division(a, b));
-function division(a, b) {
-	if (b != 0) {
-		return a/b;
-	} else {
-		return "Division durch 0 nicht möglich!";
+function division(a, b) { // Ausnahme + Abbruch                                   
+	if (b == 0) {
+		return ERROR_STR_DIV;
 	}
+	return a/b; // Regel
 }
 
 // module: output | test:
